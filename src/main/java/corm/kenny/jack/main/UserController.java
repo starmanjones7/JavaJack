35b4f25ace55bef970083a5303194e99942c3279
starmanjones7 <starmanjones7@users.noreply.github.com>
2017-10-14 14:58:38 -0700

    ctrler impl

--- src/main/java/corm/kenny/jack/main/UserController.java
+++ src/main/java/corm/kenny/jack/main/UserController.java
@@ -3,10 +3,14 @@ package corm.kenny.jack.main;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequestMapping("/users")
 @Controller
@@ -23,10 +27,22 @@ public class UserController {
 		return afoo;
 	}
 	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void createUser(@RequestBody User user) {
+		userDao.saveUser(user);
+	}
+	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
-	public List<User> getUsers() {
-		
+	public List<User> getUsersList() {
+		return userDao.getUserList();
+	}
+	
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	@ResponseBody
+	public User getUser(@PathVariable("userId")String id) {
+		return userDao.getUser(id);
 	}
 	
 }
