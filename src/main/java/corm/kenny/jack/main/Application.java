b19d3f332c5be751fc6a3a2fb8fa277c66964831
Kenny <kennywcorman@gmail.com>
2017-10-14 11:37:53 -0700

    Initial scaffolding

---
+++ src/main/java/corm/kenny/jack/main/Application.java
@@ -0,0 +1,19 @@
+package corm.kenny.jack.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import corm.kenny.jack.types.Board;
+
+@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages={"corm"})
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
